928b773352510fb441db4ebe8de27ba577453855
anshulis-sys <bhiwapurkaranshuli@gmail.com>
2026-03-10 10:21:21 +0530

    Add files via upload

---
+++ # taking input from user.py
@@ -0,0 +1,68 @@
+# taking input from user
+age = input("Enter you age:")
+# ag1 = int(input("Enter value ag1:")) type casting 
+print(age)
+print(type(age))
+
+# Find yoir current age
+dob =  int(input("Enter you date of birth"))
+current_year = 2026
+age = current_year - dob
+print("Your current age is",age)
+
+# Calculate area of rectangle
+l = float(input("lenght:"))
+b = float(input("Breadth:"))
+area = l*b
+print(area)
+
+# celsius to fahrenheit 
+c = int(input("Enter temperature in celsius"))
+f = (c * 9/5) + 32
+print("Temperature in f is:",f)
+
+# Calculate simple interest
+p = int(input("Enter the principle value"))
+r = int(input("Enter rate:"))
+t = int(input("Enter time:"))
+sI = p*r*t/100
+print("Simple Interest is ",sI)
+
+# Swap to numbers 
+a = int(input("Enter value of a :"))
+b = int(input("Enter value of b"))
+temp = a
+a = b
+b = temp
+print(a)
+print(b)
+
+# Find square of a number
+a = int(input("Enter value of a : "))
+sq = a*a
+print(sq)
+cube = a**3
+print(cube)
+
+# minutes to hours 
+m = int(input("Enter time in minutes: "))
+h = m / 60 
+print("Time in hours is : ", h)
+
+# Calculate average of three numbers 
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number two"))
+num3 = int(input("enter num three"))
+av = (num1+num2+num3)/3
+print(av)
+
+# Convert km to m
+d = int(input("Enter distance in km: "))
+m = d * 1000
+print("Distance in m is ",m)
+
+# Convert rs to $
+
+# Calculate total marks to percentage cgpa 
+
+# Calculate 
---
+++ # variable rules.py
@@ -0,0 +1,19 @@
+# variable rules
+# 123abc = 1000 //invalid
+
+# Boolean (True/False)
+# Example :
+is_student = True
+is_logged_in = False
+
+# Convert number to string 
+num = 100
+print(type(num))
+text = str(num)
+print(type(text))
+
+# addition
+num1 = "10"
+num2 = "20"
+sum = int(num1)+int(num2)
+print(sum)
